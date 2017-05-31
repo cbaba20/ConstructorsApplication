442c5d7577af276dae70be7364dcad48d8fd08ba
cbaba20 <cbaba20@gmail.com>
2017-05-31 23:16:41 +0530

    changes1

--- DemoApplicationConstructors/Program.cs
+++ DemoApplicationConstructors/Program.cs
@@ -8,8 +8,23 @@ namespace DemoApplicationConstructors
 {
     class Program
     {
+        //A special method of the class that will be automatically invoked 
+        //when an instance of the class is created is called a constructor. 
+
+        //use of constructors is to initialize private fields of the class while creating an instance for the class.
+
+        int a, b;
+        public Program()
+        {
+            a = 100;
+            b = 75;
+        }
         static void Main(string[] args)
         {
+            Program p = new Program();
+            Console.WriteLine(p.a);
+            Console.WriteLine(p.b);
+            Console.ReadLine();
         }
     }
 }
